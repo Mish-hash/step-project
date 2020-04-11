f424e225a89f8184eb25a4e311e1ec8692a6796c
Mish-hash <poluektov23@gmail.com>
2020-04-11 09:22:50 +0300

    new less

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,22 @@
 <h1>Home</h1>
 
-<?php showMessage();?>
+<?php //showMessage();?>
+
+<?php 
+    $sliders = glob('images/*', GLOB_ONLYDIR); //images/animals
+
+    foreach($sliders as $slider) {
+        $images = glob($slider . '/150x150_*');
+        print_r($images);
+
+        echo '<div class="bg-secondary my-3 slider">';
+        foreach($images as $img) {
+            echo '<img src="'.$img.'">';
+        }
+        echo '</div>';
+    }
+
+    
+?>
 
 
